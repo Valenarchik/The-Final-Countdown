0ba3814f54d77360575c8b80b82854df667615f5
Pavel <pavel.mokr2005@gmail.com>
2024-03-30 14:50:57 +0500

    AddGameMaster

---
+++ Assets/CountDown/Scripts/GameMaster.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CountDown
+{
+    public class GameMaster: MonoBehaviour
+    {
+        [SerializeField] private EventTime[] events;
+
+        public void StartGame()
+        {
+            foreach (var @event in events)
+            {
+                RegisterEvent(@event);
+            }
+        }
+        
+        private void RegisterEvent(EventTime @event)
+        {
+            StartCoroutine(DelayInvoke(@event));
+        }
+
+        private IEnumerator DelayInvoke(EventTime @event)
+        {
+            yield return new WaitForSeconds(@event.timeInMinutes * 60);
+            @event.Event.Invoke();
+        }
+    }
+    
+    [Serializable]
+    public class EventTime
+    {
+        public UnityEvent Event;
+        public float timeInMinutes;
+    }
+}
--- Assets/CountDown/Scripts/GameRoot.cs
+++ Assets/CountDown/Scripts/GameRoot.cs
@@ -1,12 +1,29 @@
-﻿using DataStructures;
+﻿using System;
+using DataStructures;
 using UnityEngine;
 
 namespace CountDown
 {
     public class GameRoot: Singleton<GameRoot>
     {
-        public Map Map;
-        public Player Player1;
-        public Player Player2;
+        [Header("Players")]
+        [SerializeField] private Player player1;
+        [SerializeField] private Player player2;
+
+        [Header("Controllers")] 
+        [SerializeField] private GameMaster gameMaster;
+        
+        public Player Player1 => player1;
+        public Player Player2 => player2;
+
+        private void Start()
+        {
+            StartGame();
+        }
+
+        public void StartGame()
+        {
+            gameMaster.StartGame();
+        }
     }
 }
