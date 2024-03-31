dc3d275a4f41086f712bcb848d3de4efdb6d1fcd
Pavel <pavel.mokr2005@gmail.com>
2024-03-31 20:32:49 +0500

    Build

---
+++ Assets/CountDown/Scripts/ExitButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CountDown
+{
+    [RequireComponent(typeof(Button))]
+    public class ExitButton: MonoBehaviour
+    {
+        private Button button;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            button.onClick.AddListener(OnClick);
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/CountDown/Scripts/SceneLoadButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace CountDown
+{
+    [RequireComponent(typeof(Button))]
+    public class SceneLoadButton: MonoBehaviour
+    {
+        private Button button;
+        [SerializeField] private SceneName sceneName;
+        
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            button.onClick.AddListener(OnClick);
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            SceneManager.LoadScene((int) sceneName);
+        }
+    }
+}
